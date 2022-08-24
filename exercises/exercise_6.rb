cfab4151eea9348009e51dcd572581fea5a110d9
donnahuang <donnaphzj@gmail.com>
2022-08-24 12:14:28 -0400

    finish exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+store1=Store.find_by(id:1)
+store2=Store.find_by(id:2)
+store4=Store.find_by(id:4)
+store5=Store.find_by(id:5)
+
+store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+store2.employees.create(first_name: "Donna", last_name: "Huang", hourly_rate: 100)
+
+store4.employees.create(first_name: "Milky", last_name: "Ho", hourly_rate: 80)
+
+store4.employees.create(first_name: "CoCo", last_name: "Cola", hourly_rate: 00)
+
+store5.employees.create(first_name: "Banana", last_name: "Nana", hourly_rate: 90)
