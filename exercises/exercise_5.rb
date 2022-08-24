9acead746a6d178dd09eb4d53adbd91e6f164329
donnahuang <donnaphzj@gmail.com>
2022-08-24 11:44:13 -0400

    finish exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts " here is the total number of revenue #{Store.sum(:annual_revenue)}"
+puts "here is the acerafe annual revenue #{Store.sum(:annual_revenue)/Store.count}"
+stores=Store.where.not(annual_revenue:0..1000000)
+puts "number of stores that are generating $1M or more in annual sales #{stores.count}"
